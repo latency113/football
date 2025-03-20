96d18a9cad9512732ae442b2b6bb7dd4ec4e217f
Khan <49835781+latency113@users.noreply.github.com>
2025-03-21 00:57:16 +0700

    Update SignupForm.jsx

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -8,7 +8,7 @@ const SignupForm = ({ onSignup }) => {
     e.preventDefault();
     if (!name) return;
 
-    await axios.post("http://localhost:3000/signup", {
+    await axios.post("https://footballbackend-vqs7.onrender.com/signup", {
       name,
       activity: "Football",
     });
